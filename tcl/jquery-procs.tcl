88d6a2f8c3203ee793a7e61b0b9794090d1081fc
Michael Aram <michael@aram.at>
2011-10-20 18:46:13 +0200

    Initial way too bare version...

---
+++ tcl/jquery-procs.tcl
@@ -0,0 +1,27 @@
+ad_library {
+    jQuery for OpenACS
+
+    @author Michael Aram
+}
+
+namespace eval ::jquery {
+
+    ad_proc -public version {} {
+        You can use this procedure to get the currently used jQuery version
+        number on your system.
+    } {
+        # TODO
+        return 1.6.4
+    }
+
+    ad_proc -public src {
+        {-minified:boolean}
+    } {
+        Use this function to get the "right" jQuery version on your system.
+        @return Returns a URL that can be used to include the jQuery library.
+    } {
+        # TODO: Use a package parameter to include an arbitrary jQuery lib.
+        return "/resources/jquery/jquery-1.6.4.min.js"
+    }
+
+}
